75bfdc6a9354dbe3e71b8b0d8cd416171d278a57
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-09-07 20:28:17 +0100

    simple movement

--- server/server.py
+++ server/server.py
@@ -2,9 +2,10 @@
 """
 Simple Redis server using a Pub/sub comunication, and JSON for the messages serialization.
 """
+import datetime
+from functools import partial
 import json
 import logging
-import pprint
 
 import logzero
 import redis
@@ -25,6 +26,14 @@ class ScapiServer():
         self.logger = self._setup_logging()
 
         self.setup_game(TEAM_NAMES, board_id=BOARD_ID)
+        self.process_action_mapping = {
+            'right': partial(self.process_action_right, value=1),
+            'left': partial(self.process_action_right, value=-1),
+            'up': partial(self.process_action_down, value=-1),
+            'down': partial(self.process_action_down, value=1),
+            # 'drop': self.process_action_drop,
+            # 'open': self.process_action_open,
+        }
 
     def _setup_logging(self):
         log_format = (
@@ -38,32 +47,40 @@ class ScapiServer():
     def setup_teams(self, team_names):
         self.teams = {}
         for team_id, name in enumerate(team_names, 1):
+            spawn_coordinates = [team_id, team_id]
             self.teams[name] = {
                 'id': team_id,
                 'score': 0,
-                'coordinates': [0, 0],
+                'coordinates': spawn_coordinates,
                 'last_action': None,
                 'left_maze': False,
                 'exit_time': None,
             }
+            self.update_team_on_board(team_id, spawn_coordinates, spawn_coordinates)
 
     def setup_game(self, team_names, board_id):
-        self.setup_teams(team_names)
         self.board = [
             ['#', '#', '#', '#', '#', '#', '#'],
-            ['#', '.', '.', '.', '.', '5', '#'],
-            ['#', '.', '.', '.', '4', '.', '#'],
-            ['#', '.', '.', '3', '.', '.', '#'],
-            ['#', '.', '2', '.', '.', '.', '#'],
-            ['#', '1', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '#'],
             ['#', '#', '#', '-', '#', '#', '#'],
         ]
-        self.game_state = {
-            'last_teams_action': [None for i in range(len(self.teams))],
-            'team_scores': [0 for i in range(len(self.teams))]
-        }
+        self.setup_teams(team_names)
+
+    def update_team_on_board(self, team_id, old_coord, new_coord):
+        self.board[old_coord[0]][old_coord[1]] = '.'
+        self.board[new_coord[0]][new_coord[1]] = str(team_id)
+
+    def show_updated_screen(self):
+        self.show_board()
+        self.show_last_actions()
+        self.show_scores()
 
     def run(self):
+        self.show_updated_screen()
         pubsub = self.redis_db.pubsub(ignore_subscribe_messages=True)
         pubsub.subscribe('scapi')
         for message in pubsub.listen():
@@ -73,31 +90,73 @@ class ScapiServer():
                 self.logger.error(f'Error processing {message}:')
                 self.logger.exception(e)
             finally:
-                self.show_board()
-                self.show_state()
+                self.show_updated_screen()
+
+    def is_valid_board_move_coordinate(self, coordinates):
+        try:
+            return self.board[coordinates[0]][coordinates[1]] == '.'
+        except IndexError:
+            return False
+
+    def move_team_if_valid(self, team, old_coordinates, new_coordinates):
+        if self.is_valid_board_move_coordinate(new_coordinates):
+            team_id = self.teams[team]['id']
+            self.teams[team]['coordinates'] = new_coordinates
+            self.update_team_on_board(team_id, old_coordinates, new_coordinates)
+
+    def process_action_right(self, team, value):
+        old_coordinates = self.teams[team]['coordinates']
+        new_coordinates = [old_coordinates[0], old_coordinates[1] + value]
+        self.move_team_if_valid(team, old_coordinates, new_coordinates)
+
+    def process_action_down(self, team, value):
+        old_coordinates = self.teams[team]['coordinates']
+        new_coordinates = [old_coordinates[0] + value, old_coordinates[1]]
+        self.move_team_if_valid(team, old_coordinates, new_coordinates)
+
+    def process_action(self, team, user, action):
+        action_mapping = self.process_action_mapping.get(action)
+        if action_mapping is None:
+            return False
+        action_mapping(team)
 
     def process_msg(self, json_msg):
         msg_data = json.loads(json_msg)
+        team = msg_data['team']
+        user = msg_data['user']
+        action = msg_data['action']
+        self.process_action(team, user, action)
+        self.teams[team]['last_action'] = msg_data
 
         self.logger.debug(msg_data)
 
     def show_board(self):
+        print('\n' * 100)
         for row in self.board:
             print(''.join(row))
 
     def show_last_actions(self):
-        last_teams_action = self.game_state['last_teams_action']
-        print(f'Last Team Action:')
-        for team_index, team_action in enumerate(last_teams_action):
-            team = self.teams[team_index]
-            user = team_action['user']
-            action = team_action['action']
-            print(f'\t {team}/{user}: {action}')
+        print(f'Last Team Action Received:')
+        for team, team_data in self.teams.items():
+            last_action = team_data['last_action']
+            if last_action:
+                user = last_action['user']
+                action = last_action['action']
+                print(f'\t {team}/{user}: {action}')
+            else:
+                print(f'\t {team}/: None')
 
-    def show_state(self):
-        # teams_left = self.game_state['teams_left']
-        # print(f'Teams Scores: {teams_left}')
-        self.show_last_actions()
+    def show_scores(self):
+        print(f'Team Scores:')
+        for team, team_data in self.teams.items():
+            team_id = team_data['id']
+            left_maze = team_data['left_maze']
+            score = team_data['score']
+            if left_maze:
+                exit_time = team_data['exit_time']
+                print(f'\t ({team_id}){team}\t({exit_time}):\t{score}')
+            else:
+                print(f'\t ({team_id}){team}\t (On Going):\t{score}')
 
 
 if __name__ == '__main__':
--- server/test_msg.py
+++ server/test_msg.py
@@ -5,7 +5,7 @@ import redis
 from conf import REDIS_ADDRESS, REDIS_PORT
 
 
-def send_messages(redis_db, msg_data):
+def send_message(redis_db, msg_data):
     json_msg = json.dumps(msg_data)
     redis_db.publish('scapi', json_msg)
 
@@ -13,8 +13,32 @@ def send_messages(redis_db, msg_data):
 if __name__ == '__main__':
     redis_db = redis.Redis(host=REDIS_ADDRESS, port=REDIS_PORT)
     msg_data = {
-        'user': 'someone',
-        'team': 'TheTop25',
+        'user': 'someone1',
+        'team': 'PythonTamers',
         'action': 'left'
     }
-    send_messages(redis_db, msg_data)
+    send_message(redis_db, msg_data)
+
+    msg_data = {
+        'user': 'someone2',
+        'team': 'PythonTamers',
+        'action': 'right'
+    }
+
+    send_message(redis_db, msg_data)
+
+    msg_data = {
+        'user': 'student1',
+        'team': 'SeeMoreAndMore',
+        'action': 'up'
+    }
+
+    send_message(redis_db, msg_data)
+
+    msg_data = {
+        'user': 'student2',
+        'team': 'Disassembly',
+        'action': 'down'
+    }
+
+    send_message(redis_db, msg_data)
