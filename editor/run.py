aa0cfa746f844a5ed9ce32b0728130878cf7756b
Annanda Sousa <annanda.sousa@insight-centre.org>
2021-10-02 18:05:13 +0100

    Creating a board edit window, to edit the board.

--- editor/run.py
+++ editor/run.py
@@ -21,18 +21,28 @@ class Editor:
                   [sg.Button('Create New')]]
         self.window = sg.Window(f'{self.title} - Main Menu', layout)
 
-    def create_board_options_window(self):
-        sg.theme('DarkAmber')
-        layout = [[sg.Text('Main Menu')],
-                  [sg.Text('Height:')],
-                  [sg.Text('Width:')],
-                  [sg.Button('Go')]]
-        self.window = sg.Window(f'{self.title} - Board Options', layout)
-
     def process_event(self, event, values):
         if event == 'Create New':
             self.window.close()
             self.create_board_options_window()
+
+        if event == 'Create':
+            self.window.close()
+            self.create_board_edit_window()
+
+    def create_board_options_window(self):
+        layout = [[sg.Text('Main Menu')],
+                  [sg.Text('Width:'), sg.InputText()],
+                  [sg.Text('Height:'), sg.InputText()],
+                  [sg.Button('Create')]]
+        sg.theme('DarkAmber')
+        self.window = sg.Window(f'{self.title} - Board Options', layout)
+
+    def create_board_edit_window(self):
+        layout = [[sg.Text('Edit Board')],
+                  [sg.Button('Save')]]
+        sg.theme('DarkAmber')
+        self.window = sg.Window(f'{self.title} - Edit Board', layout)
 
     def run(self):
         # Event Loop to process "events"
