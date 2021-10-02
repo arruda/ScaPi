7f930d1cd83d1b3c80aac25dc5fa4e61a0fcfd23
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-10-02 17:26:47 +0100

    separating run method

--- editor/run.py
+++ editor/run.py
@@ -5,23 +5,29 @@ import PySimpleGUI as sg
 class Editor:
     # Create the Window
     def __init__(self):
-        self.create_windows()
+        self.create_window()
 
-    def create_windows(self):
+    def create_window(self):
         sg.theme('DarkAmber')  # Add a little color to your windows
         # All the stuff inside your window. This is the PSG magic code compactor...
         layout = [[sg.Text('Some text on Row 1')],
                   [sg.Text('Enter something on Row 2'), sg.InputText()],
                   [sg.OK(), sg.Cancel()]]
-        window = sg.Window('Window Title', layout)
+        self.window = sg.Window('Window Title', layout)
 
+    def run(self):
         # Event Loop to process "events"
-        while True:
-            event, values = window.read()
-            if event in (sg.WIN_CLOSED, 'Cancel'):
-                break
-        window.close()
+        try:
+            while True:
+                event, values = self.window.read()
+                if event in (sg.WIN_CLOSED, 'Cancel'):
+                    break
+        except Exception:
+            pass
+        finally:
+            self.window.close()
 
 
 if __name__ == '__main__':
     my_editor = Editor()
+    my_editor.run()
