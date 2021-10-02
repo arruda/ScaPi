875c755e9b7452e086ba25c11b86a28602e88a77
Annanda Sousa <annanda.sousa@insight-centre.org>
2021-10-02 18:15:37 +0100

    Using create window method with default args to create similar windows

--- editor/run.py
+++ editor/run.py
@@ -3,23 +3,24 @@ import PySimpleGUI as sg
 
 
 class Editor:
+    DEFAULT_SIZE = (640, 480)
+
     def __init__(self):
+        sg.theme('DarkAmber')
         self.title = 'ScaPi Editor'
         self.create_main_menu()
 
-    def create_window(self):
-        sg.theme('DarkAmber')
-        layout = [[sg.Text('Some text on Row 1')],
-                  [sg.Text('Enter something on Row 2'), sg.InputText()],
-                  [sg.OK(), sg.Cancel()]]
-        self.window = sg.Window(f'{self.title} - Main Window', layout)
+    def create_window(self, sub_title, layout, size=DEFAULT_SIZE):
+        self.window = sg.Window(f'{self.title} - {sub_title}', layout, size=size)
+        return self.window
 
     def create_main_menu(self):
-        sg.theme('DarkAmber')
+        # sg.theme('DarkAmber')
         layout = [[sg.Text('Main Menu')],
                   [sg.Text('Available Options:')],
                   [sg.Button('Create New')]]
-        self.window = sg.Window(f'{self.title} - Main Menu', layout)
+        self.create_window('Main Menu', layout)
+        # self.window = sg.Window(f'{self.title} - Main Menu', layout)
 
     def process_event(self, event, values):
         if event == 'Create New':
@@ -35,14 +36,13 @@ class Editor:
                   [sg.Text('Width:'), sg.InputText()],
                   [sg.Text('Height:'), sg.InputText()],
                   [sg.Button('Create')]]
-        sg.theme('DarkAmber')
-        self.window = sg.Window(f'{self.title} - Board Options', layout)
+
+        self.create_window('Board Options', layout)
 
     def create_board_edit_window(self):
         layout = [[sg.Text('Edit Board')],
                   [sg.Button('Save')]]
-        sg.theme('DarkAmber')
-        self.window = sg.Window(f'{self.title} - Edit Board', layout)
+        self.create_window('Edit Board', layout)
 
     def run(self):
         # Event Loop to process "events"
