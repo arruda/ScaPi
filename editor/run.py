3b47026ac5862ad7c2dd40a8f31ac696b09a14b2
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-10-02 17:36:56 +0100

    creating a main menu

--- editor/run.py
+++ editor/run.py
@@ -4,20 +4,29 @@ import PySimpleGUI as sg
 
 class Editor:
     def __init__(self):
-        self.create_window()
+        self.title = 'ScaPi Editor'
+        self.create_main_menu()
 
     def create_window(self):
         sg.theme('DarkAmber')
         layout = [[sg.Text('Some text on Row 1')],
                   [sg.Text('Enter something on Row 2'), sg.InputText()],
                   [sg.OK(), sg.Cancel()]]
-        self.window = sg.Window('Window Title', layout)
+        self.window = sg.Window(f'{self.title} - Main Window', layout)
+
+    def create_main_menu(self):
+        sg.theme('DarkAmber')
+        layout = [[sg.Text('Main Menu')],
+                  [sg.Text('Available Options:')],
+                  [sg.Button('Testing')]]
+        self.window = sg.Window(f'{self.title} - Main Menu', layout)
 
     def run(self):
         # Event Loop to process "events"
         try:
             while True:
                 event, values = self.window.read()
+                print(f'{event}, {values}')
                 if event in (sg.WIN_CLOSED, 'Cancel'):
                     break
         except Exception:
