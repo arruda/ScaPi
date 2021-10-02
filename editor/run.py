8b705294f3404c1c91a542194f2f77b492fdea52
Annanda Sousa <annanda.sousa@insight-centre.org>
2021-10-02 17:58:13 +0100

    Creating a board option window, to configure the dimensions of the board.

--- editor/run.py
+++ editor/run.py
@@ -18,8 +18,21 @@ class Editor:
         sg.theme('DarkAmber')
         layout = [[sg.Text('Main Menu')],
                   [sg.Text('Available Options:')],
-                  [sg.Button('Testing')]]
+                  [sg.Button('Create New')]]
         self.window = sg.Window(f'{self.title} - Main Menu', layout)
+
+    def create_board_options_window(self):
+        sg.theme('DarkAmber')
+        layout = [[sg.Text('Main Menu')],
+                  [sg.Text('Height:')],
+                  [sg.Text('Width:')],
+                  [sg.Button('Go')]]
+        self.window = sg.Window(f'{self.title} - Board Options', layout)
+
+    def process_event(self, event, values):
+        if event == 'Create New':
+            self.window.close()
+            self.create_board_options_window()
 
     def run(self):
         # Event Loop to process "events"
@@ -29,6 +42,7 @@ class Editor:
                 print(f'{event}, {values}')
                 if event in (sg.WIN_CLOSED, 'Cancel'):
                     break
+                self.process_event(event, values)
         except Exception:
             pass
         finally:
