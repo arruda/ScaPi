1fa00654b635481a1b0818b46153724b9e2e5949
Annanda Sousa <annanda.sousa@insight-centre.org>
2021-10-02 17:05:34 +0100

    Creating the structure to start the board Editor GUI.

---
+++ editor/run.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import PySimpleGUI as sg
+
+
+class Editor:
+    # Create the Window
+    def __init__(self):
+        self.create_windows()
+
+    def create_windows(self):
+        sg.theme('DarkAmber')  # Add a little color to your windows
+        # All the stuff inside your window. This is the PSG magic code compactor...
+        layout = [[sg.Text('Some text on Row 1')],
+                  [sg.Text('Enter something on Row 2'), sg.InputText()],
+                  [sg.OK(), sg.Cancel()]]
+        window = sg.Window('Window Title', layout)
+
+        # Event Loop to process "events"
+        while True:
+            event, values = window.read()
+            if event in (sg.WIN_CLOSED, 'Cancel'):
+                break
+        window.close()
+
+
+if __name__ == '__main__':
+    my_editor = Editor()
